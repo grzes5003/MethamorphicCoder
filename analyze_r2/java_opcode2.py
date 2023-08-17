e2ce696606bba7eb26bb84d0442cb92e7b3f253e
grzes5003 <grzes.kulinski@gmail.com>
2023-08-17 21:11:15 +0200

    more java results

--- analyze_r2/java_opcode2.py
+++ analyze_r2/java_opcode2.py
@@ -31,79 +31,63 @@ def extract_instructions(project_root, class_name):
         return f"Error: {e.output}"
 
 
-def compare_opcode_sequences(seq_x, seq_y):
+def compare_opcodes(opcodes_X, opcodes_Y):
     matches = defaultdict(list)
-
-    for i in range(len(seq_x) - 2):
-        for j in range(len(seq_y) - 2):
-            s1 = seq_x[i:i + 3]
-            s2 = seq_y[j:j + 3]
-            if set(s1) == set(s2):
-                matches[i].append(j)
-
+    for i in range(len(opcodes_X) - 2):
+        subseq_X = tuple(opcodes_X[i:i+3])
+        for j in range(len(opcodes_Y) - 2):
+            subseq_Y = tuple(opcodes_Y[j:j+3])
+            if subseq_X == subseq_Y:
+                matches[subseq_X].append((i, j))
     return matches
 
 
-def calculate_similarity(matches, n, m, threshold):
-    grid = [[0] * m for _ in range(n)]
+def calculate_similarity(opcodes_X, opcodes_Y, threshold=5):
+    matches = compare_opcodes(opcodes_X, opcodes_Y)
+    graph = [[0] * len(opcodes_Y) for _ in range(len(opcodes_X))]
 
-    for x, y_list in matches.items():
-        for y in y_list:
-            grid[x][y] += 1
+    for match_positions in matches.values():
+        if len(match_positions) >= threshold:
+            for x, y in match_positions:
+                graph[x][y] = 1
 
-    similarity_score = 0
+    covered_X = sum(any(row) for row in graph)
+    covered_Y = sum(any(graph[i][j] for i in range(len(opcodes_X))) for j in range(len(opcodes_Y)))
 
-    for i in range(n):
-        for j in range(m):
-            if grid[i][j] >= threshold:
-                similarity_score += 1
-
-    similarity_score /= (n + m)
-
-    return similarity_score
+    similarity_score = (covered_X / len(opcodes_X) + covered_Y / len(opcodes_Y)) / 2
+    return similarity_score, graph
 
 
-def plot_opcode_matrix(matches, n, m, title: str = 'Opcode Matches Matrix',
-                       x_lab: str = "Program Y Opcode Number",
-                       y_lab: str = "Program X Opcode Number"):
-    matrix = np.zeros((n, m))
-
-    for x, y_list in matches.items():
-        for y in y_list:
-            matrix[x, y] = 1
-
-    plt.figure(figsize=(10, 8))  # Set the figure size (adjust as needed)
-    plt.imshow(matrix, cmap='gray', interpolation='none', aspect='auto', origin='lower')
-    plt.xlabel(x_lab)
-    plt.ylabel(y_lab)
-    plt.title(title)
+def plot_similarity_matrix(matrix, x_ticks, y_ticks, x_labels = '(Program Y)'):
+    plt.figure(figsize=(10, 10))
+    plt.imshow(matrix, cmap='gray', aspect='auto', origin='lower')
     plt.colorbar()
-
+    plt.xticks(range(len(y_ticks)), y_ticks, rotation=45, ha='right')
+    plt.yticks(range(len(x_ticks)), x_ticks)
+    plt.xlabel(x_labels)
+    plt.ylabel("base version")
+    plt.title("Filtered Opcode Similarity Matrix (threshold=2)")
+    plt.tight_layout()
     plt.show()
 
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument('-i', '--input_file', type=str, help='Path to the input file', required=True)
-    argparser.add_argument('-b', '--input_file2', type=str, help='Path to the input file', required=True)
     args = argparser.parse_args()
 
     # Specify the bytecode filename and class name
     bytecode_filename = PATH_ROOT / args.input_file
-    bytecode_filename2 = PATH_ROOT / args.input_file2
     class_name = "org.example.Main"
-
     instructions = extract_instructions(bytecode_filename, class_name)
-    instructions2 = extract_instructions(bytecode_filename2, class_name)
 
-    matches = compare_opcode_sequences(instructions, instructions2)
+    for i in range(2, 6):
+        bytecode_filename2 = str(bytecode_filename).replace("1", str(i))
+        program_name = bytecode_filename2.split('\\')[-3]
+        instructions2 = extract_instructions(bytecode_filename2, class_name)
+        score, graph = calculate_similarity(instructions, instructions2, threshold=2)
+        print(f'{program_name}: {score:.2f}')
+        plot_similarity_matrix(graph, instructions, instructions2, x_labels=f'Program "{program_name}"')
 
-    plot_opcode_matrix(matches, len(instructions), len(instructions2),
-                       title='Opcode Matches Matrix for "Quick Sort" java version',
-                       x_lab="base version",
-                       y_lab="version q_sort5")
-
-    print(instructions)
-    print(instructions2)
     # Extract and print the instructions
 
